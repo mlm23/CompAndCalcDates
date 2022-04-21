a0901f82e81688c1299b1e7d2121ba6559e3ce5e
mlm23 <f23o11d2001@gmail.com>
2022-04-21 13:28:01 +0100

    reorgranización de inclui e innui con cambios de nombres en las clases

---
+++ innui/Calculos.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inclui.CompAndCalcDates
+{
+     public class Calculos
+    {
+       /// <summary>
+       /// Se encarga de calcular la diferencia entre la fecha que ha introducido el usuario hasta hoy, calculando la diferencia entre años y dias
+       /// </summary>
+       /// <param name="Fecha"></param>
+       /// <param name="Periodo"></param>
+       /// <param name="FechaActual"></param>
+       /// <returns> La cadena  que  representa los resultados obtenidos</returns>
+        public static string DiferenciaEntreHoy(List<int> Fecha, int Periodo, List<int> FechaActual)
+        {
+            string linea = "";
+            int DiferenciaAños = HallarAños(Periodo, FechaActual, Fecha);
+            int DiferenciaDias = HallarDiasHastaHoy(FechaActual, Fecha, Periodo,DiferenciaAños);
+            //si la difencia es negativa , hacemos el valor absoluto para que la diferencia sea positiva
+            DiferenciaAños = Math.Abs(DiferenciaAños);
+            DiferenciaDias = Math.Abs(DiferenciaDias);
+            //el string linea sirve concatenar el texto y los calculos realizados anteriormente para posteriormente mostrarlo por pantalla
+            linea += " La diferencia entre la Fecha Actual  " + DiferenciaAños + " en años y con días " + DiferenciaDias;
+            return linea;
+        }
+        /// <summary>
+        ///  Se encarga de calcular la diferencia entre la fechas que se han introducido por usuario , calculando la diferencia entre años y dias
+        /// </summary>
+        /// <param name="Fecha1"></param>
+        /// <param name="Fecha2"></param>
+        /// <param name="Periodo1"></param>
+        /// <param name="Periodo2"></param>
+        /// <param name="FechaActual"></param>
+        /// <returns> La cadena  que  representa los resultados obtenidos</returns>
+        public static string DiferenciasEntreFechas(List<int> Fecha1, List<int> Fecha2, int Periodo1, int Periodo2)
+        {
+            string cadena = "";
+            int DiferenciaAños = CalcularDiferenciaAños(Periodo1, Periodo2, Fecha1, Fecha2);
+            int DiferenciaDias = CalcularDiferenciaDias(Fecha1, Fecha2, DiferenciaAños);
+            DiferenciaAños = Math.Abs(DiferenciaAños);
+            DiferenciaDias = Math.Abs(DiferenciaDias);
+            cadena += " La diferencia entre ambas Fechas es " + DiferenciaAños + " en años y en días " + DiferenciaDias;
+            return cadena;
+        }
+
+        /// <summary>
+        /// Calcula la diferencia en años entre las fechas con la fecha actual
+        /// </summary>
+        /// <param name="Periodo"></param>
+        /// <param name="FechaActual"></param>
+        /// <param name="Fecha"></param>
+        /// <returns> El valor de la diferencia</returns>
+        public static int HallarAños(int Periodo, List<int>FechaActual, List<int>Fecha)
+        {
+            int años = 0;
+            //Si la fecha introducida es después de Cristo, la diferencia sería restando los años
+            if(Periodo==1)
+            {
+                años = FechaActual[0] - Fecha[0];
+            }
+            //Si la fecha introducida es antes de Cristo, la diferencia sería sumando los años
+            if (Periodo==0)
+            {
+                años = FechaActual[0] + Fecha[0];
+            }
+            return años;
+        }
+        /// <summary>
+        /// Calcula el total de días que han transcurrido entre la fecha introducida a la fecha actual
+        /// </summary>
+        /// <param name="FechaActual"></param>
+        /// <param name="Fecha"></param>
+        /// <param name="Periodo"></param>
+        /// <returns> numero de dias totales</returns>
+        public static int HallarDiasHastaHoy(List<int> FechaActual, List<int> Fecha, int Periodo, int difenciaAnno)
+        {
+            int dias = 0;
+            //sumamos los años totales para calcular los dias totales bisiestos
+            int AñosTotales = FechaActual[0] + Fecha[0];
+            int diasBisiestos = 0;
+            //Si es en la era de Cristo
+            if(Periodo==1)
+            {
+                //recorremos hacia delante los años
+                for (int contador = Fecha[0]; contador == AñosTotales; contador++)
+                {
+                    //Por cada año bisiesto sumamos un día
+                    if (DateTime.IsLeapYear(contador))
+                    {
+                        diasBisiestos++;
+                    }
+                }
+            }
+            //Si es antes de Cristo
+            if(Periodo==0)
+            {
+                //recoremos hacia atrás
+                for (int contador = Fecha[0]; contador == AñosTotales; contador--)
+                {
+                    //Por cada año bisiesto sumamos un día
+                    if (DateTime.IsLeapYear(contador))
+                    {
+                        diasBisiestos++;
+                    }
+                }
+            }
+            //generamos un array en donde se ve la cantidad total de días por cada mes
+            int[] NDias = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int diastotales1 = 0;
+
+            //Recorremos el año actual y sumamos los días ya transcurridos en lo que llevamos de año
+            for (int contador = 0; contador < FechaActual[1]; contador++)
+            {
+                diastotales1 = diastotales1 + NDias[contador];
+            }
+            int diastotales2 = 0;
+            //Recorre el año de la fecha introducida y se suma el valor de cada mes trancurrido
+            for (int contador = 0; contador < Fecha[1]; contador++)
+            {
+                diastotales2 = diastotales2 + NDias[contador];
+            }
+            //La diferencia en años lo multiplicamos por 365, le sumamos los dias bisiestos, los dias transcurridos de la fecha de hoy, sumamos los dias del mes de la fecha actual
+            //y luego restamos los dias del mes de la fecha introducida y sus dias totales de los meses de ese año que se habian transcurridos 
+            dias = difenciaAnno* 365 + diasBisiestos + diastotales1 + FechaActual[2] - Fecha[2] - diastotales2;
+            return dias;
+        }
+        /// <summary>
+        /// Calcula la diferencia en años entre las dos fechas introducidas por consola
+        /// </summary>
+        /// <param name="periodo1"></param>
+        /// <param name="periodo2"></param>
+        /// <param name="Fecha1"></param>
+        /// <param name="Fecha2"></param>
+        /// <returns>El valor de la diferencia de años</returns>
+        public static int CalcularDiferenciaAños(int periodo1, int periodo2, List<int>Fecha1, List<int>Fecha2)
+        {
+            int añosTotales = 0;
+            if(periodo1==periodo2)//están en la misma Época , se restan los años
+            {
+                añosTotales=Fecha1[0] - Fecha2[0];
+            }
+            else
+            {
+                //si la primera fecha está en la era de Cristo y la segunda antes de cristo, ambas se restan
+                if(periodo1==1 && periodo2==0)
+                {
+                    añosTotales = Fecha1[0] + Fecha2[0];
+                }
+                else
+                {
+                    //si la primera fecha está antes de Cristo y la segunda después de cristo, ambas se suman
+                    if (periodo1==0 && periodo2==1)
+                    {
+                        añosTotales = Fecha1[0] + Fecha2[0];
+                    }
+                }
+            }
+            return añosTotales;
+        }
+        /// <summary>
+        /// El objetivo es calcular los días de difencecia entre ambas fechas introducida por el usuario
+        /// </summary>
+        /// <param name="Fecha1"></param>
+        /// <param name="Fecha2"></param>
+        /// <param name="diferenciaAnno"></param>
+        /// <returns></returns>
+        public static int CalcularDiferenciaDias(List<int> Fecha1, List<int> Fecha2, int diferenciaAnno)
+        {
+            //sumamos los años totales para calcular los dias totales bisiestos
+            int AñosTotales = Fecha1[0] + Fecha2[0];
+            int diferencias = 0;
+            int diasBisiestos = 0;
+            //recorremos los años totales y vemos a cada año bisiesto sumamos un día
+            for (int contador = Fecha1[0]; contador==AñosTotales; contador++)
+            {
+                if (DateTime.IsLeapYear(contador))
+                {
+                    diasBisiestos++;
+                }
+            }
+            //generamos un array en donde se ve la cantidad total de días por cada mes
+            int[] NDias = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            int diastotales1 = 0;
+            //Recorremos el año de la primera fecha y sumamos los días ya transcurridos en lo que llevamos de año
+            for (int contador = 0; contador < Fecha1[1]; contador++)
+            {
+                diastotales1 = diastotales1 + NDias[contador];
+            }
+            //Recorre el año de la segunda fecha y se suma el valor de cada mes trancurrido
+            int diastotales2 = 0;
+            for (int contador = 0; contador < Fecha2[1]; contador++)
+            {
+                diastotales2 = diastotales2 + NDias[contador];
+            }
+            diferencias = diferenciaAnno * 365 + diasBisiestos + diastotales1 + Fecha1[2] - Fecha2[2] - diastotales2;
+            return diferencias;
+        }
+    }
+}
---
+++ innui/Funciones.cs
@@ -0,0 +1,413 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inclui.CompAndCalcDates
+{
+    public class Funciones
+    {
+        /// <summary>
+        /// esto sirve para presentar por pantalla las fechas que el usuario ha introducido
+        /// </summary>
+        /// <param name="Fecha1"> La primera Fecha que ha introducido </param>
+        /// <param name="Fecha2"> La segunda Fecha que ha introducido </param>
+        public static string MostrarFechas(List<int> Fecha)
+        {
+            string cadena = "la fecha es " + Fecha[0]+ "-" +Fecha[1] + "-" +Fecha[2];
+            Console.WriteLine(cadena);
+            return cadena;
+        }
+        /// <summary>
+        /// su función es presentar por pantalla los cálculos realizados de las diferencias
+        /// </summary>
+        /// <param name="DiferenciaEntreFechas"> el valor de la diferencia entre las dos fechas introducidas por el usuario</param>
+        /// <param name="DiferenciaHoyFecha1"> el valor de la diferencia entre la primera fecha y la fecha actual </param>
+        /// <param name="DiferenciaHoyFecha2">  el valor de la diferencia entre la segunda fecha y la fecha actual </param>
+        public static void MostrarEdades(string DiferenciaEntreFechas, string DiferenciaHoyFecha1, string DiferenciaHoyFecha2)
+        {
+            Console.WriteLine("A continuación visualizaremos la diferencia entre las dos fechas dadas: ");
+            Console.WriteLine(DiferenciaEntreFechas);
+            Console.WriteLine("\n ");
+            Console.WriteLine("A continuación visualizaremos la diferencia entre la primera fecha y la fecha actualmente: ");
+            Console.WriteLine(DiferenciaHoyFecha1);
+            Console.WriteLine("\n ");
+            Console.WriteLine("A continuación visualizaremos la diferencia entre la segunda fecha y la fecha actualmente: ");
+            Console.WriteLine("\n ");
+            Console.WriteLine(DiferenciaHoyFecha2);
+
+        }
+        /// <summary>
+        /// Se encarga de recoger cada dato que va introduciendo por consola y a su vez la va agregando en lista
+        /// </summary>
+        /// <returns> Una lista con año, mes y día </returns>
+        public static List<int> RellenarArrays()
+        {
+            List<int> Fecha = new List<int>();
+            int anno = Funciones.SolicitarAnno();
+            Fecha.Add(anno);
+            int mes = Funciones.SolicitarMes();
+            Fecha.Add(mes);
+            bool bisiesto = Funciones.Comprobarbisiesto(anno);
+            int dia = Funciones.ComprobacionDias(anno, mes, bisiesto);
+            Fecha.Add(dia);
+            return Fecha;
+        }
+        /// <summary>
+        /// Solicitar un número para saber si la fecha está antes o después de Cristo
+        /// </summary>
+        /// <returns> El valor 0 o 1 que indica el Periodo </returns>
+        public static int SolicitarPeriodo()
+        {
+            int periodo = 0;
+            bool leido = false;
+            //Un bucle que hasta que no se verifique bien la entrada de datos no salimos por eso el booleano
+            do
+            {
+                Console.WriteLine(" Pulse 0 si el siguiente año es antes de Cristo o teclee 1 si es despúes de Cristo");
+                if (!int.TryParse(Console.ReadLine(), out periodo))
+                {
+                    Console.WriteLine("Dato no válido el periodo debe ser un número entero");
+                    Console.WriteLine("Presione una tecla para volver a intentarlo");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    //El periodo no puede ser negativo y el usuario vuelve a introducir el dato
+                    if (periodo < 0)
+                    {
+                        Console.WriteLine("El periodo no puede ser negativo");
+                        Console.WriteLine("Presione una tecla para volver a intentarlo");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        //Si es 0 sería antes de Cristo y salimos del bucle con el booleano 
+                        if (periodo == 0)
+                        {
+                            leido = true;
+                        }
+                        else
+                        {
+                            //Si es 1 es después de Cristo y salimos del bucle con el booleano 
+                            if (periodo == 1)
+                            {
+                                leido = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("No ha introducido un 0 o 1");
+                                Console.WriteLine("Presione una tecla para volver a intentarlo");
+                                Console.ReadKey();
+                            }
+
+                        }
+
+                    }
+                }
+
+            } while (!leido);
+
+            return periodo;
+        }
+        /// <summary>
+        /// Solicita el Año de la fecha 
+        /// </summary>
+        /// <returns> El valor del año de la fecha</returns>
+        public static int SolicitarAnno()
+        {
+            int anno = 0;
+            bool leido = false;
+            //no salimos hasta que la entrada de datos esté correctamente
+            do
+            {
+                Console.WriteLine("Introduza el año");
+                if (!int.TryParse(Console.ReadLine(), out anno))
+                {
+                    Console.WriteLine("Dato no válido el año tiene que ser un número entero");
+                    Console.WriteLine("Presione una tecla para volver a intentarlo");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    //el año debe ser positivo, no adminitimos en el programa datos negativos
+                    if (anno > 0)
+                    {
+                        //el limite de fechas que acepta el windows respecto a la fecha actual
+                        if (anno < 9999)
+                        {
+                            leido = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Fuera de rango");
+                            Console.WriteLine("Presione una tecla para volver a intentarlo");
+                            Console.ReadKey();
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("El año no puede ser negativo");
+                        Console.WriteLine("Presione una tecla para volver a intentarlo");
+                        Console.ReadKey();
+                    }
+                }
+
+            } while (!leido);
+            return anno;
+        }
+        /// <summary>
+        /// Solicita el mes de la fecha
+        /// </summary>
+        /// <returns> El valor del mes</returns>
+        public static int SolicitarMes()
+        {
+            int mes = 0;
+            //creo un array en donde están los nombres de los meses, el objetivo es que el usuario introduzca el nombre del mes
+            //y luego devolver con la posición del array que es lo que corresponde al número del mes
+
+            string[] meses = {"enero","febrero","marzo",
+                "abril","mayo","junio","julio","agosto",
+                    "septiembre","octubre","noviembre","diciembre"};
+            bool leido = false;
+            do
+            {
+                Console.WriteLine("Introduzca el nombre del mes");
+                string cadena = Console.ReadLine().Trim();
+                if (cadena != "")
+                {
+                    bool recorrer = false;
+                    int contador = 0;
+                    //recorro el array meses
+                    while (!recorrer)
+                    {
+                        if (contador >= meses.Length)
+                        {
+                            recorrer = true;
+                            Console.WriteLine("Nombre del mes mal introducido");
+                            Console.WriteLine("Presione una tecla para volver a intentarlo");
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            //El ToLower() es por si el usuario introduce algo en mayúscula y luego lo compara todo en minúscula con el array
+                            if (cadena.ToLower() == meses[contador])
+                            {
+                                //el mes es la posicion en donde se haya el mes en el array
+                                mes = contador + 1;
+                                recorrer = true;
+                                leido = true;
+                            }
+                        }
+                        contador++;
+                    }
+                }
+            } while (!leido);
+            return mes;
+        }
+        /// <summary>
+        /// Verificar si el año es bisiesto o no
+        /// </summary>
+        /// <param name="anno"></param>
+        /// <returns> un valor si es true es bisiesto, si es false no es bisiesto</returns>
+        public static bool Comprobarbisiesto(int anno)
+        {
+            bool comprobacion = false;
+            if (DateTime.IsLeapYear(anno))
+            {
+                comprobacion = true;
+            }
+            return comprobacion;
+        }
+        /// <summary>
+        /// Valida que el dato día este introducido correctamente respetando los requisitos
+        /// </summary>
+        /// <param name="anno"></param>
+        /// <param name="mes"></param>
+        /// <param name="bisiesto"></param>
+        /// <returns> el valor del dia de la fecha </returns>
+        public static int ComprobacionDias(int anno, int mes, bool bisiesto)
+        {
+            int dia = 0;
+            bool Correcto = false;
+            bool Error = true;
+            do
+            {
+                //llamamos a la función que se introduce el número del dia 
+                dia = IntroducirDia(mes, anno, dia);
+                //En el caso que el mes sea febrero
+                if (mes == 2)
+                { 
+                    //LLamamos a la funcion que chequea el dia esté entre los limites establecidos
+                    DiaFebrero(bisiesto, dia, ref Error);
+                    //comprobamos que no se haya producido un error y si no hay error salimos del bucle
+                    if (!Error)
+                    {
+                        Correcto = true;
+                    }
+                }
+                else
+                {
+                    //vemos si el mes es par o impar 
+                    if (mes % 2 != 0)
+                    {
+                        //LLamamos a la funcion para verificar que el valor de dia esté entre los limites establecidos (max 31)
+                        MesImpar(ref Error, dia);
+                        //comprobamos que no se haya producido un error y si no hay error salimos del bucle
+                        if (!Error)
+                        {
+                            Correcto = true;
+                        }
+                    }
+                    else
+                    {
+                        //LLamamos a la funcion para verificar que el valor de dia esté entre los limites establecidos (max 30)
+                        MesPar(ref Error, dia);
+                        //comprobamos que no se haya producido un error y si no hay error salimos del bucle
+                        if (!Error)
+                        {
+                            Correcto = true;
+                        }
+                    }
+                }
+
+
+            } while (!Correcto);
+            return dia;
+        }
+        /// <summary>
+        /// Se introduce por consola el numero del dia de la fecha
+        /// </summary>
+        /// <param name="mes"></param>
+        /// <param name="anno"></param>
+        /// <param name="dia"></param>
+        /// <returns></returns>
+        public static int IntroducirDia(int mes, int anno, int dia)
+        {
+
+            bool leido = false;
+            do
+            {
+                Console.WriteLine("Introduzca el dia del {0} del mes {1}", anno, mes);
+                if (!Int32.TryParse(Console.ReadLine(), out dia))
+                {
+                    Console.WriteLine("Dato no válido el dia tiene que ser un número entero");
+                    Console.WriteLine("Presione una tecla para volver a intentarlo");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else
+                {
+                    //dia tiene que ser positivo, no se admite en el programa datos negativos
+                    if (dia > 0)
+                    {
+                        leido = true;
+                    }
+                    else
+
+                    {
+                        Console.WriteLine("El dia tiene que ser positivo");
+                        Console.WriteLine("Presione una tecla para volver a intentarlo");
+                        Console.ReadKey();
+                    }
+
+                }
+
+            } while (!leido);
+            return dia;
+        }
+        /// <summary>
+        /// Se encarga de verificar que dia esté entre los limites que son si es bisiesto 29 max y si no 28 max
+        /// </summary>
+        /// <param name="bisiesto"></param>
+        /// <param name="dia"></param>
+        /// <param name="Error"></param>
+        public static void DiaFebrero(bool bisiesto, int dia, ref bool Error)
+        {
+           
+            if (bisiesto)
+            {
+                if (dia <= 29)
+                {
+                    Error = false;
+                }
+                else
+                {
+                    Console.WriteLine("Dato no válido el dia tiene que ser menor al máximo o igual  que es 29");
+                    Console.WriteLine("Presione una tecla para volver a intentarlo");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Error = true;
+                }
+            }
+            else
+            {
+                if (dia <= 28)
+                {
+                    Error = false;
+                }
+                else
+                {
+                    Console.WriteLine("Dato no válido el dia tiene que ser menor al máximo o igual  que es 28");
+                    Console.WriteLine("Presione una tecla para volver a intentarlo");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Error = true;
+                }
+            }
+        }
+        public static void MesImpar(ref bool Error, int dia)
+        {
+           //en los meses impares del año los días son hasta 31
+            if (dia <= 31)
+            {
+                Error = false;
+            }
+            else
+            {
+                Console.WriteLine("Dato no válido el dia tiene que ser menor al máximo o igual que es 31");
+                Console.WriteLine("Presione una tecla para volver a intentarlo");
+                Console.ReadKey();
+                Console.Clear();
+                Error = true;
+            }
+        }
+       
+        public static void MesPar(ref bool Error, int dia)
+        {
+            //en los meses pares del año los días son hasta 30
+            if (dia <= 30)
+            {
+                Error = false;
+            }
+            else
+            {
+                Console.WriteLine("Dato no válido el dia tiene que ser menor al máximo o igual que es 30");
+                Console.WriteLine("Presione una tecla para volver a intentarlo");
+                Console.ReadKey();
+                Console.Clear();
+                Error = true;
+            }
+        }
+        /// <summary>
+        /// Se encarga de hallar la fecha actual para luego hacer los cálculos, se recoge la información y se introduce en un  lista
+        /// </summary>
+        /// <returns> La lista de la fecha actual </returns>
+        public static List<int> ObtenerFechaActual()
+        {
+            List<int> Fecha = new List<int>();
+            //ponemos el formato que nos interesa para hacer los cálculos más sencillos
+            string Date = DateTime.Now.ToString("yyyy-MM-dd");
+            //dividimos por guión y lo ponemos en un array con el objetivo de sacar cada campo y luego añadirlo en la lista en su order correspondiente
+            string[] dividir = Date.Split('-');
+            foreach (string paso in dividir)
+            {
+                Int32.TryParse(paso, out int numero);
+                Fecha.Add(numero);
+            }
+            return Fecha;
+        }
+    }
+}
